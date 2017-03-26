f4a51b841857fe25e8655783fd5ab7aa4ff9fca7
t_hayashi <t.hayashi0077+github@gmail.com>
2017-03-26 14:52:55 +0900

    Initial Commit

---
+++ AnchorKit/AnchorKit.h
@@ -0,0 +1,19 @@
+//
+//  AnchorKit.h
+//  AnchorKit
+//
+//  Created by 林達也 on 2017/03/26.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnchorKit.
+FOUNDATION_EXPORT double AnchorKitVersionNumber;
+
+//! Project version string for AnchorKit.
+FOUNDATION_EXPORT const unsigned char AnchorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnchorKit/PublicHeader.h>
+
+
